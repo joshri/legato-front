3acd316f8e6a5ba4ca82490763de564ebb008146
Joshua Israel <joshisrael93@gmail.com>
2020-12-02 17:21:47 -0500

    email page draft for AWS test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-  
+      <h1>Want to receive updates on Legato?</h1>
+      <label for='email'>Email :</label>
+      <input type='email' placeholder='example@example.com'></input>
     </div>
   );
 }
