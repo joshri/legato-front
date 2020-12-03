64a3519cbe8362a799c9eca246e129d3d4bb3f44
Joshua Israel <joshisrael93@gmail.com>
2020-12-03 16:57:15 -0500

    reset for transfer to dev

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '../App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from '../aws-exports';
-import SignUpForm from './SignUpForm';
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <SignUpForm />
+      
       <h1>Want to receive updates on Legato?</h1>
       <label for='email'>Email :</label>
       <input type='email' placeholder='example@example.com'></input>
