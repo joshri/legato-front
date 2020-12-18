3bfbee7f9691366e2e65778b76f683d1718bcd8f
Joshua Israel <joshisrael93@gmail.com>
2020-12-18 12:20:13 -0500

    initial sass code

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
-import '../Landing.scss';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../scss/Landing.scss';
 import React, { useState, useEffect } from 'react';
 import Amplify from 'aws-amplify';
 import awsconfig from '../aws-exports';
@@ -8,33 +9,46 @@ import ConnectMessage from './ConnectMessage';
 Amplify.configure(awsconfig);
 
 function App() {
-	
+
+	document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01);
+
 	return (
 		<div className='App'>
-			<ConnectMessage />
+			<div className='connect-msg'>
+				<ConnectMessage />
+			</div>
 			<Mailchimp
 				action='https://legatoconnects.us2.list-manage.com/subscribe/post?u=29fc24c88a25afe7251ee87a8&amp;id=8b8af18ae4'
 				fields={[
 					{
 						name: 'EMAIL',
-						placeholder: 'Email',
+						placeholder: 'email',
 						type: 'email',
 						required: true,
 					},
 					{
 						name: 'FNAME',
-						placeholder: 'First Name',
+						placeholder: 'first name',
 						type: 'text',
 						required: true,
 					},
 					{
 						name: 'LNAME',
-						placeholder: 'Last Name',
+						placeholder: 'last name',
 						type: 'text',
 						required: true,
 					},
 				]}
+				messages={{
+					sending: 'Sending...',
+					success: 'Thank you for subscribing!',
+					error: 'An unexpected error has occurred.',
+					empty: 'Please enter an email address.',
+					duplicate: "You're already on our list!",
+					button: 'count me in!',
+				}}
 				className='mailchimp'
+				buttonClassName='mailchimp-button'
 			/>
 		</div>
 	);
--- src/Components/ConnectMessage.js
+++ src/Components/ConnectMessage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 function ConnectMessage(props) {
 	const connections = {
@@ -10,32 +10,35 @@ function ConnectMessage(props) {
 	let [message, setMessage] = useState(0);
 
 	useEffect(() => {
-	
 		setTimeout(() => {
 			if (message < 1) {
 				setMessage(message + 1);
 			} else {
 				setMessage(0);
 			}
-			
 		}, 3000);
 	});
 
 	return (
-		<div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-			<h1>Legato connects </h1>
-			<div>
+		<div className='msg-line'>
+			<h1>connecting </h1>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-evenly',
+					width: '50vw',
+				}}>
 				<motion.h1
-					animate={{ opacity: [0, 1, 1, 0], translateY: [0, 20, 20, 40] }}
+				style={{marginRight: '20vw'}}
+					animate={{ opacity: [0, 1, 1, 1, 0, 0], translateY: [0, 20, 20, 40] }}
 					transition={{ repeat: Infinity, duration: 3 }}
 					className='message'>
 					{connections[message][0]}
 				</motion.h1>{' '}
-			</div>
-			<h1>to </h1>
-			<div>
+				<h1>to </h1>
 				<motion.h1
-					animate={{ opacity: [0, 1, 1, 0], translateY: [0, 20, 20, 40] }}
+				style={{marginLeft:'20vw'}}
+					animate={{ opacity: [0, 1, 1, 1, 0, 0], translateY: [0, 20, 20, 40] }}
 					transition={{ repeat: Infinity, duration: 3 }}
 					className='message'>
 					{connections[message][1]}
